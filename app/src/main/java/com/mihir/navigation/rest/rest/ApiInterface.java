e46c093e910e5f76b34d2887ed620b94a3bb2528
Mihir <mhrpatel12@gmail.com>
2017-07-04 03:02:55 +0530

    retrofit package

---
+++ app/src/main/java/com/mihir/navigation/rest/rest/ApiInterface.java
@@ -0,0 +1,17 @@
+package com.mihir.navigation.rest.rest;
+
+import com.mihir.navigation.rest.model.DirectionResults;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+
+public interface ApiInterface {
+
+    @GET("/maps/api/directions/json")
+    Call<DirectionResults> getJson(@Query("origin") String origin, @Query("destination") String destination, @Query("mode") String transportMode, @Query("key") String apiKey);
+}
